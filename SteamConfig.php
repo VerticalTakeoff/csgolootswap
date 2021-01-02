4270ac517e4d683802ef210cbfad3817e04a0f3f
VerticalTakeoff <41929166+VerticalTakeoff@users.noreply.github.com>
2021-01-01 16:49:06 -0800

    Update SteamConfig.php

--- SteamConfig.php
+++ SteamConfig.php
@@ -2,8 +2,8 @@
 //Version 4.0
 $steamauth['apikey'] = "32DED241C8191251A8AC15313D304FF1"; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
 $steamauth['domainname'] = "https://csgolootswap.com/"; // The main URL of your website displayed in the login page
-$steamauth['logoutpage'] = "index.php"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
-$steamauth['loginpage'] = "index.php"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['logoutpage'] = "index.html"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['loginpage'] = "index.html"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 
 // System stuff
 if (empty($steamauth['apikey'])) {die("<div style='display: block; width: 100%; background-color: red; text-align: center;'>SteamAuth:<br>Please supply an API-Key!<br>Find this in steamauth/SteamConfig.php, Find the '<b>\$steamauth['apikey']</b>' Array. </div>");}
